511e5386246bc774869a8b3b176428a5a9d7f6e1
Elias <18127101+eliasfloreteng@users.noreply.github.com>
2025-10-30 03:16:46 +0000

    Add schedule to database

--- app/src/main/java/se/floreteng/bundl/BundlDatabase.kt
+++ app/src/main/java/se/floreteng/bundl/BundlDatabase.kt
@@ -6,13 +6,16 @@ import se.floreteng.bundl.apprule.AppRule
 import se.floreteng.bundl.apprule.AppRuleDao
 import se.floreteng.bundl.notifications.Notification
 import se.floreteng.bundl.notifications.NotificationDao
+import se.floreteng.bundl.schedule.Schedule
+import se.floreteng.bundl.schedule.ScheduleDao
 
 @Database(
-    entities = [AppRule::class, Notification::class],
-    version = 2,
+    entities = [AppRule::class, Notification::class, Schedule::class],
+    version = 3,
     exportSchema = false
 )
 abstract class BundlDatabase : RoomDatabase() {
     abstract val appRuleDao: AppRuleDao
     abstract val notificationDao: NotificationDao
+    abstract val scheduleDao: ScheduleDao
 }
